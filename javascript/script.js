8abd09bbd0bcdf76e540f4d5bfbd32e79b7a6a67
dipesh goswami <dipeshgoswami1999@gmail.com>
2023-11-29 17:59:48 +0530

    javascript work completed

--- javascript/script.js
+++ javascript/script.js
@@ -39,7 +39,7 @@ const questions = [
 
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("next");
+const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
@@ -54,17 +54,18 @@ function showQuestion() {
   let currentQuestion = questions[currentQuestionIndex];
   let questionNo = currentQuestionIndex + 1;
   questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
-  currentQuestion.answers.forEach(answers => {
+
+  currentQuestion.answers.forEach(answer => {
     const button = document.createElement("button");
-    button.innerHTML = answers.title;
+    button.innerHTML = answer.title;
     button.classList.add("btn");
     answerButtons.appendChild(button);
-    if (answers.correct) {
-      button.dataset.correct = answes.correct;
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
 
     }
     button.addEventListener("click", selectAnswer);
-  })
+  });
 }
 
 function resetState() {
@@ -80,9 +81,48 @@ function selectAnswer(e) {
   const isCorrect = selectedBtn.dataset.correct === "true";
   if (isCorrect) {
     selectedBtn.classList.add("correct");
+    score++;
   }
   else {
     selectedBtn.classList.add("incorrect");
   }
+  Array.from(answerButtons.children).forEach(button => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+
+  });
+  nextButton.style.display = "block";
 }
+
+
+
+function showScore() {
+  resetState();
+  questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+  nextButton.innerHTML = "Play Again!"
+  nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  }
+  else {
+    showScore();
+  }
+}
+
+
+
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    handleNextButton();
+  }
+  else {
+    startQuiz();
+  }
+});
 startQuiz();
